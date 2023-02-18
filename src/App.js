69480cf418f49ba2661bbba9fe37e6339821597e
BrakeLightBoy <michellarsson962@gmail.com>
2023-02-18 20:57:22 +0100

    Add Base Home Page
    Added a basic home page with external links for contact.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import linkedInLogo from './icons/LinkedIn_icon.svg';
+import githubLogo from './icons/github_icon.svg';
+import gmailLogo from './icons/gmail_icon.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Michael Larsson</h1>
+        <h2>Who am I?</h2>
+        <h3>I'm an aspiring Software Developer currently studying a bachelors in Software Engineering & Management.</h3>
+        <h2>Contact Me</h2>
+        <div>
+        <a href="https://www.linkedin.com/in/michael-larsson-368499214/" target={'_blank'}>
+          <img 
+          src = {linkedInLogo} 
+          alt = "LinkedInLogo"
+          style={{ height: 53, width: 36, padding:30}}/>
         </a>
+        <a href="https://github.com/BrakeLightBoy" target={'_blank'}>
+          <img 
+          src = {githubLogo} 
+          alt = "GithubLogo"
+          style={{ height: 53, width: 53, padding:30}}/>
+        </a>
+        <a href="mailto:michellarsson962@gmail.com" target={'_blank'}>
+          <img 
+          src = {gmailLogo} 
+          alt = "Gmail"
+          style={{ height: 50, width: 50, padding:30}}/>
+        </a>
+        </div>
       </header>
     </div>
   );
