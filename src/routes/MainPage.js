535b8e282979594423af4d09bd610fd66c1455e9
michlars <michellarsson962@gmail.com>
2023-04-03 14:04:55 +0200

    style: add base for home page

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -7,36 +7,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Michael Larsson</h1>
-        <h2>Who am I?</h2>
-        <h3>I'm an aspiring Software Developer currently studying a bachelors in Software Engineering & Management.</h3>
-        <h2>Contact Me</h2>
-        <div>
-        <a href="https://www.linkedin.com/in/michael-larsson-368499214/" target={'_blank'}>
-          <img 
-          src = {linkedInLogo} 
-          alt = "LinkedInLogo"
-          style={{ height: 53, width: 36, padding:30}}/>
-        </a>
-        <a href="https://github.com/BrakeLightBoy" target={'_blank'}>
-          <img 
-          src = {githubLogo} 
-          alt = "GithubLogo"
-          style={{ height: 53, width: 53, padding:30}}/>
-        </a>
-        <a href="mailto:michellarsson962@gmail.com" target={'_blank'}>
-          <img 
-          src = {gmailLogo} 
-          alt = "Gmail"
-          style={{ height: 50, width: 50, padding:30}}/>
-        </a>
-        <div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
-
-<div>
-  <p style={{width: '70px', textAlign: 'center'}}>Example</p>
-</div>
-
-<div style={{flex: 1, height: '1px', backgroundColor: 'black'}} />
+        <div className='row page-row'>
+          <div className='col col-1'>
+            <h1 id='headerTitle'>Michael<br></br>Larsson</h1>
+          </div>
+          <hr
+              style={{
+              background: "#F2C230",
+              height: "1px",
+              border: "none",
+              opacity: 1
+              }}
+            />
+            <div className='row main-title-row'>
+              <div className='col col-12 about-me'>
+              <h1 id='about-title' className='main-page-text'>ABOUT <span id='me-text'>ME</span></h1>
+              </div>
+            </div>
+            <div className='row main-title-row'>
+              <div className='col col-12 about-me'>
+              <h1 id='project-title' className='main-page-text'>MY <span id='project-text'>PROJECTS</span></h1>              </div>
+            </div>
+            <div className='row main-title-row'>
+              <div className='col col-12 about-me'>
+              <h1 id='contact-title' className='main-page-text'>CONTACT <span id='me-text'>ME</span></h1>              </div>
+            </div>
         </div>
       </header>
     </div>
