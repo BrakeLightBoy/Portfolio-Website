70ca99b3edd47c3177154535e7295d765fbe3e72
BrakeLightBoy <michellarsson962@gmail.com>
2024-11-05 19:43:39 +0100

    refactor: replace original repository content with updated personal-portfolio files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import logo from './programming.gif';
 import './App.css';
+import './components/Project';
+import Project from './components/Project';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Michael Larsson</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Project></Project>
     </div>
   );
 }
